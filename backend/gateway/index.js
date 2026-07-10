277d12ecca83e69ae08c8e430146718cb8b2f5e6
AnshulGarg2004 <ganshul2004@gmail.com>
2026-07-10 15:49:28 +0530

    gateway setup

---
+++ backend/gateway/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/connectDB.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    return res.status(200).json({message : "setup gateway complete"})
+})
+
+app.listen(port, async () => {
+    await connectDB();
+    console.log("gateway started at: ", port);
+    
+})
